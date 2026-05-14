53d796367052eaa9abcf85c1666e2742e5a3e939
Arne Ludwig <arne.ludwig@posteo.de>
2026-05-14 21:46:36 +0200

    feat: Bootstrap-styled forms via bs_field tag; Edit in card dropdown menu

--- chores/templatetags/chore_tags.py
+++ chores/templatetags/chore_tags.py
@@ -1,4 +1,6 @@
+from django import forms as django_forms
 from django import template
+from django.utils.safestring import mark_safe
 from xp.formulas import calculate_xp
 
 register = template.Library()
@@ -8,6 +10,25 @@ register = template.Library()
 def xp_preview(instance):
     settings = instance.owner.profile.xp_settings
     return calculate_xp(instance.definition.base_xp, instance.streak_count, settings)
+
+
+@register.inclusion_tag("_bs_field.html")
+def bs_field(field):
+    widget = field.field.widget
+    errors = field.errors
+    if isinstance(widget, (django_forms.Select, django_forms.SelectMultiple)):
+        css_class = "form-select"
+    elif isinstance(widget, django_forms.CheckboxInput):
+        css_class = "form-check-input"
+    else:
+        css_class = "form-control"
+    if errors:
+        css_class += " is-invalid"
+    return {
+        "field": field,
+        "rendered_widget": mark_safe(field.as_widget(attrs={"class": css_class})),
+        "is_checkbox": isinstance(widget, django_forms.CheckboxInput),
+    }
 
 
 @register.filter
