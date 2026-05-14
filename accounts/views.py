04b157a333aba930e6da7f7a6083147897b47b8f
Arne Ludwig <arne.ludwig@posteo.de>
2026-05-14 22:44:57 +0200

    feat: add profile view and URL

--- accounts/views.py
+++ accounts/views.py
@@ -1,7 +1,12 @@
-from django.contrib.auth import login
+from django.contrib.auth import login, update_session_auth_hash
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.forms import PasswordChangeForm
+from django.contrib import messages
+from django.shortcuts import redirect, render
 from django.urls import reverse_lazy
 from django.views.generic import CreateView
-from .forms import RegisterForm
+
+from .forms import PersonalInfoForm, RegisterForm
 
 
 class RegisterView(CreateView):
@@ -13,3 +18,31 @@ class RegisterView(CreateView):
         response = super().form_valid(form)
         login(self.request, self.object)
         return response
+
+
+@login_required
+def profile(request):
+    user = request.user
+    info_form = PersonalInfoForm(instance=user)
+    password_form = PasswordChangeForm(user)
+
+    if request.method == "POST":
+        action = request.POST.get("action")
+        if action == "info":
+            info_form = PersonalInfoForm(request.POST, instance=user)
+            if info_form.is_valid():
+                info_form.save()
+                messages.success(request, "Personal info updated.")
+                return redirect("profile")
+        elif action == "password":
+            password_form = PasswordChangeForm(user, request.POST)
+            if password_form.is_valid():
+                password_form.save()
+                update_session_auth_hash(request, password_form.user)
+                messages.success(request, "Password changed.")
+                return redirect("profile")
+
+    return render(request, "accounts/profile.html", {
+        "info_form": info_form,
+        "password_form": password_form,
+    })
